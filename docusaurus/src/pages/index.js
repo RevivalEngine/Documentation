1cf87d6aa68e548f50480861e477af49f776f630
RDW <duckwhale@mail.ru>
2021-07-02 03:29:24 +0200

    UI: Reworked landing page to allow quick access to the docs
    For the initial version, having a shortcut to developer info is more important than the about/features page (as there isn't much to feature yet).

--- docusaurus/src/pages/index.js
+++ docusaurus/src/pages/index.js
@@ -1,40 +1,65 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<header className={clsx("hero hero--primary", styles.heroBanner)}>
+			<div className="container">
+				<h1 className="hero__title">
+					Welcome to the {siteConfig.title}!
+				</h1>
+				<p className="hero__subtitle">
+					{siteConfig.tagline}
+				</p>
+				<p>
+					<a class="contrasted-link" href="/about/">
+						If you're new: Click here to learn more about the project!
+					</a>
+				</p>
+				<div className={styles.buttons} id="landing-page-button-container">
+					<Link className="button landing-page-button button--secondary button--lg" to="/docs/tutorials">
+						<h1>Tutorials</h1>
+						Get started in minutes.
+					</Link>
+
+					<Link className="button landing-page-button button--secondary button--lg" to="/docs/recipes">
+						<h1>Recipes</h1>
+						Learn how to solve common problems.
+					</Link>
+
+					<Link className="button landing-page-button button--secondary button--lg" to="/docs/concepts">
+						<h1>Concepts</h1>
+						Understand how stuff works.
+					</Link>
+
+					<Link className="button landing-page-button button--secondary button--lg" to="/docs/references">
+						<h1>References</h1>
+						Look up technical details.
+					</Link>
+
+					<div className="EngineDocumentation_SocialsContainer">
+						Something else?
+						<br />
+						<a class="contrasted-link" href="https://discord.gg/QMbSGen9uc" target="blank">
+							Let us know on Discord!
+						</a>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+	return (
+		<Layout title={`Welcome`} description="Documentation for the RevivalEngine">
+			<HomepageHeader />
+			<main />
+		</Layout>
+	);
 }
