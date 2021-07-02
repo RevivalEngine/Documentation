8a0b6228e992d7abb6cb16af998adf8d5d298a45
RDW <duckwhale@mail.ru>
2021-07-02 03:48:03 +0200

    UI: Removed pointless blurb from the footer

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -37,47 +37,8 @@ module.exports = {
       disableSwitch: true,
     },
     footer: {
-      style: 'dark',
+      style: 'light',
       links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
       ],
       copyright: `Copyright © 2019-${new Date().getFullYear()} RevivalEngine Team and Contributors: Anonymous Citizens of the Internet`,
     },
