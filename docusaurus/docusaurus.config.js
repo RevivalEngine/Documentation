041f36f1c3036cf0e37c38e1d76166a6f151a732
RDW <duckwhale@mail.ru>
2021-07-02 04:22:16 +0200

    UI: Updated sidebar repository links
    Where are they even displayed? Can't see them anywhere.

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -54,15 +54,13 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/RevivalEngine/Documentation/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/RevivalEngine/Documentation/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
